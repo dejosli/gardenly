019d539b1af2773262616f69318819a4e1ce1a82
Josli Shubho Biswas <bjoslishubho@gmail.com>
2022-01-06 20:44:40 +0600

    update security policy and add compression

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // external imports
 require('dotenv').config();
 const path = require('path');
+const compression = require('compression');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoStore = require('connect-mongo');
@@ -26,6 +28,34 @@ const {
 
 // init express app
 const app = express();
+
+// init helmet
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// Compress all HTTP responses
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // Will not compress responses, if this header is present
+    return false;
+  }
+  // Resort to standard compression
+  return compression.filter(req, res);
+};
+
+app.use(
+  compression({
+    // filter: Decide if the answer should be compressed or not,
+    // depending on the 'shouldCompress' function above
+    filter: shouldCompress,
+    // threshold: It is the byte threshold for the response
+    // body size before considering compression, the default is 1 kB
+    threshold: 0,
+  })
+);
 
 // logger - morgan
 const loggerFormat =
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,9 @@ let mix = require('laravel-mix');
 mix
   .js('resources/js/app.js', 'public/js/app.js')
   .sass('resources/scss/app.scss', 'public/css/app.css');
+
+if (!mix.inProduction()) {
+  mix.webpackConfig({
+    devtool: 'inline-source-map',
+  });
+}
